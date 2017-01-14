c839c2e6352f49d1fe57b7093fcba2dc4391d507
Drew Westphal <drewwestphal@users.noreply.github.com>
2017-01-14 18:30:03 -0500

    generally working with wordpress container, most optimizations applied

--- wordpress/wp-salts.sh
+++ wordpress/wp-salts.sh
@@ -5,7 +5,7 @@ SALTFILE=${1-"$WORDPRESS_SALTS_PATH"}
 OWNERGROUP=${2-root:www-data}
 PERMS=${3-664}
 
-echo echo "<?php" >  "$SALTFILE"
+echo "<?php" >  "$SALTFILE"
 curl https://api.wordpress.org/secret-key/1.1/salt/ >> "$SALTFILE"
 echo "?>" >>  "$SALTFILE"
 
