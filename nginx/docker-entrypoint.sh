3a9d56e9349d40fcc8c6d2c8e20dfbf8c2a5a01e
Drew Westphal <drewwestphal@users.noreply.github.com>
2017-06-14 11:42:05 -0400

    switch to envtpl and remove helper files for envsubst

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 # do envsubst automatically for all .template files
 #inspired by https://hub.docker.com/_/nginx/
-for TEMPLATE in $(find /etc/nginx/ -name '*.template'); do
-    DEST="$(echo "$TEMPLATE" | sed "s/.template/.conf/g")"
+for TEMPLATE in $(find /etc/nginx/ -name '*.conf.ngxtpl'); do
+    DEST="$(echo "$TEMPLATE" | sed "s/\.ngxtpl//g")"
     echo "Substituting for [$TEMPLATE] to make [$DEST]..."
-    cat "$TEMPLATE" | envsubst > "$DEST"
+    envtpl < "$TEMPLATE" > "$DEST"
 done
 
 exec "$@"
